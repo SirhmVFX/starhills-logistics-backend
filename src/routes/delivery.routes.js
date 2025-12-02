2caa78e32ec5bb42de5ed0f06024a761cbb3334e
sirhmvfx <sirhmvfx@gmail.com>
2025-12-02 18:35:26 +0100

    fixed routes

--- src/routes/delivery.routes.js
+++ src/routes/delivery.routes.js
@@ -4,6 +4,7 @@ import {
   createDelivery,
   createShipment,
   getDeliveryDetails,
+  getShippingRates,
   rateDelivery,
   trackDelivery,
 } from "../controllers/delivery.controller.js";
@@ -13,6 +14,7 @@ const router = express.Router();
 router.post("/deliveries", authMiddleware, createDelivery);
 router.get("/deliveries/track", authMiddleware, trackDelivery); // Changed to query param
 router.post("/deliveries/:id/shipments", authMiddleware, createShipment);
+router.post("/deliveries/rates", authMiddleware, getShippingRates);
 router.get("/deliveries/:id", authMiddleware, getDeliveryDetails);
 router.delete("/deliveries/:id", authMiddleware, cancelDelivery);
 router.post("/deliveries/:id/rate", authMiddleware, rateDelivery);
