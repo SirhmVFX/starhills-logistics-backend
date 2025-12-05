8bb736f1da650e60b580ba30b7529204d1b6b52d
sirhmvfx <sirhmvfx@gmail.com>
2025-12-05 17:06:55 +0100

    mod server.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,19 @@ import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
-import riderRoutes from "./routes/rider.routes.js";
-import rideRoutes from "./routes/ride.routes.js";
-import deliveryRoutes from "./routes/delivery.routes.js";
+import addressRoutes from "./routes/address.routes.js";
+import ratesRoutes from "./routes/rates.routes.js";
+import courierRoutes from "./routes/courier.routes.js";
+import packageRoutes from "./routes/package.routes.js";
+import shipmentRoutes from "./routes/shipment.routes.js";
+import trackingRoutes from "./routes/tracking.routes.js";
+import returnsRoutes from "./routes/return.routes.js";
+import webhooksRoutes from "./routes/webhooks.routes.js";
+import notificationsRoutes from "./routes/notifications.routes.js";
+import reportsRoutes from "./routes/reports.routes.js";
+import supportRoutes from "./routes/support.routes.js";
 import walletRoutes from "./routes/wallet.routes.js";
-import promotionRoutes from "./routes/promotion.routes.js";
+
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./docs/swagger.js";
 import cookieParser from "cookie-parser";
@@ -22,11 +30,18 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
-app.use("/api/v1/rider", riderRoutes);
-app.use("/api/v1/ride", rideRoutes);
-app.use("/api/v1/delivery", deliveryRoutes);
 app.use("/api/v1/wallet", walletRoutes);
-app.use("/api/v1/promotion", promotionRoutes);
+app.use("/api/addresses", addressRoutes);
+app.use("/api/rates", ratesRoutes);
+app.use("/api/couriers", courierRoutes);
+app.use("/api/packages", packageRoutes);
+app.use("/api/shipment", shipmentRoutes);
+app.use("/api/tracking", trackingRoutes);
+app.use("/api/returns", returnsRoutes);
+app.use("/api/webhooks", webhooksRoutes);
+app.use("/api/notifications", notificationsRoutes);
+app.use("/api/reports", reportsRoutes);
+app.use("/api/support", supportRoutes);
 
 app.use(
   "/api-docs",
