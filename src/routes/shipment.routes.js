9814a743d9c7186fee6dac8a5c83966ffe0ac752
sirhmvfx <sirhmvfx@gmail.com>
2025-12-21 07:47:10 +0100

    fix

--- src/routes/shipment.routes.js
+++ src/routes/shipment.routes.js
@@ -7,14 +7,15 @@ import {
   getShipmentStatistics,
   getShipmentWaybill,
 } from "../controllers/shipment.controllers.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create", createShipment);
-router.get("/", getShipments);
-router.get("/:shipmentId", getShipmentById);
-router.delete("/:shipmentId", cancelShipment);
-router.get("/:shipmentId/waybill", getShipmentWaybill);
-router.get("/statistics", getShipmentStatistics);
+router.post("/create", authMiddleware, createShipment);
+router.get("/", authMiddleware, getShipments);
+router.get("/:shipmentId", authMiddleware, getShipmentById);
+router.delete("/:shipmentId", authMiddleware, cancelShipment);
+router.get("/:shipmentId/waybill", authMiddleware, getShipmentWaybill);
+router.get("/statistics", authMiddleware, getShipmentStatistics);
 
 export default router;
