10d0da03566eabb63f44e6776f7d8106aa8dfe2e
sirhmvfx <sirhmvfx@gmail.com>
2025-12-22 19:16:13 +0100

    fix

--- src/routes/wallet.routes.js
+++ src/routes/wallet.routes.js
@@ -2,12 +2,15 @@ import express from "express";
 import {
   getWalletBalance,
   getWalletTransactions,
+  handleFundingWebhook,
   requestFund,
 } from "../controllers/wallet.controller.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.post("/balance", getWalletBalance);
-router.post("/fund-request", requestFund);
-router.post("/transactions", getWalletTransactions);
+router.get("/balance", authMiddleware, getWalletBalance);
+router.post("/fund-request", authMiddleware, requestFund);
+router.post("/transactions", authMiddleware, getWalletTransactions);
+router.post("/webhook/funding", handleFundingWebhook);
 
 export default router;
